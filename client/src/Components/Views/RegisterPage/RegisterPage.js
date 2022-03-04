c9a7a4f0f6639413ddb1efaf0b2d8c9679f435b4
권민주 <momo@gwonminjuui-MacBookAir.local>
2022-03-04 17:36:47 +0900

    Feat: test2

---
+++ client/src/Components/Views/RegisterPage/RegisterPage.js
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+import RegisterOrEdit from "./Sections/RegisterOrEdit";
+
+function RegisterPage() {
+    const [TitleValue, setTitleValue] = useState("")
+    const [ContentValue, setContentValue] = useState("")
+    const [IsForUpdate, setIsForUpdate] = useState(false)
+    
+    const onTitleChange = (event) => {
+        setTitleValue(event.currentTarget.value)
+    }
+    console.log(TitleValue);
+    
+    const onContentChange = (event) => {
+        setContentValue(event.currentTarget.value)
+    }
+    console.log(ContentValue)
+
+    const onSubmitArticle = (event) => {
+        event.preventDefault();
+        const article = {title: TitleValue, content: ContentValue}
+        console.log(article);
+    }
+
+    return (
+        <div>
+        <RegisterOrEdit
+        titleValue={TitleValue}
+        contentValue = {ContentValue}
+        handleTitleChange={onTitleChange}
+        handleContentChange={onContentChange}
+        handleSubmit={onSubmitArticle}
+        updateRequest={IsForUpdate}
+        />
+        </div>
+    )
+}
+
+export default RegisterPage;
