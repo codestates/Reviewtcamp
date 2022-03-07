fb09b706b98575a1da02d4453fad30a90670503d
권민주 <momo@gwonminjuui-MacBookAir.local>
2022-03-07 14:18:42 +0900

    Fix: Router error solved

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,21 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom'
-
+import { Link, Route, BrowserRouter, Switch} from 'react-router-dom'
+import NavBar from './Components/NavBar';
 import RegisterPage from './Components/Views/RegisterPage/RegisterPage';
 import ArticlePage from './Components/Views/ArticlePage/ArticlePage';
 import Home from './Pages/Home';
 import BoardPage from './Components/Views/BoardPage/BoardPage';
 function App() {
   return (
-    <div className='App'>
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/article/:id">
-        <ArticlePage />
-      </Route>
-      <Route path="/board">
-        <BoardPage />
-      </Route>
-      <Route path="/register">
-        <RegisterPage />
-      </Route>
-    </Switch>
-  </div>
-
+<div>
+        <NavBar />
+        <Switch> <Route exact path="/"><Home /></Route>
+       
+         <Route exact path="/register"><RegisterPage /></Route>
+        <Route exact path="/article"><ArticlePage /></Route>
+        <Route exact path="/board"><BoardPage /></Route>
+</Switch>
+</div>
   );
 }
 
