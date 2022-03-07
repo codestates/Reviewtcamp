b66dcbbeba759c9827b4fb231804e91057027a55
권민주 <momo@gwonminjuui-MacBookAir.local>
2022-03-07 10:25:50 +0900

    Feat: Add Board, Register page

---
+++ client/src/Components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+function NavBar() {
+    return (
+        <div>
+        <button>로고 들어가는 자리</button>
+        <button>마이페이지</button>
+        <button>로그아웃</button>
+        </div>
+    )
+}
+
+export default NavBar
---
+++ client/src/Components/Views/ArticlePage/ArticlePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+function ArticlePage() {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default ArticlePage
