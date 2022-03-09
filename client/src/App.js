cd7dc7b45fe380cba22f6111d15d84e57db65d6c
Best-engineer <kkm6101@gmail.com>
2022-03-09 11:18:20 +0900

    Feat: css function

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import Signup from "./Pages/Signup";
 import Signin from "./Pages/Signin";
 import Mypage from "./Pages/Mypage";
+import Modal from "./Pages/Modal";
 import BoardList from "./Pages/BoardList";
 import BoardPage from "./Pages/BoardPage";
 import ArticlePage from "./Pages/ArticlePage";
@@ -15,39 +16,19 @@ import RegisterPage from "./Pages/RegisterPage";
 export default function App() {
   const [isLogin, setIsLogin] = useState(false);
   const [accessToken, setAccessToken] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+
   const loginHandler = (data) => {
     setIsLogin(true);
     setAccessToken(data.token.accessToken);
   };
 
-  // ! New AccessToken발급
-  useEffect(() => {
-    async function checkRefreshToKen() {
-      // ! New AccessToken 어디서 발급 받나? 엔드포인드?
-      const result = await await fetch("https://reviewtcamp.com/refreshtoken", {
-        method: "POST",
-        credentials: "include",
-        headers: {
-          "Content-type": "application/json",
-        },
-      });
-      setAccessToken({
-        accesstoken: result.accessToken,
-      });
-      // setLoading(false);
-    }
-    setLoading(false);
-    checkRefreshToKen();
-  }, []);
-
   if (loading) return <div>Loading...</div>;
 
   return (
-    <div>   
-    <NavBar />
+    <div>
+      <NavBar />
       <Switch>
-   
         <Route exact path="/signin">
           <Signin loginHandler={loginHandler} />
         </Route>
@@ -58,50 +39,29 @@ export default function App() {
 
         <Route exact path="/mypage">
           <Mypage accessToken={accessToken} />
-          {/* 로그인이된 상황이면 -> 토큰 받아와서 mypage에서 사용자 정보시 사용하기 */}
-          {/* {isLogin ? (
-            <Mypage accessToken={accessToken} />
-          ) : (
-            <Redirect to="/" />
-          )} */}
         </Route>
 
-        <Route exact path="/"> 
+        <Route exact path="/">
           {isLogin ? <Redirect to="/mypage" /> : <Redirect to="/signin" />}
-    
-          </Route>
+        </Route>
 
-          <Route exact path="/home">
+        <Route exact path="/modal">
+          <Modal />
+        </Route>
+
+        <Route exact path="/home">
           <Home />
         </Route>
-      <Route exact path="/article/:id">
-        <ArticlePage />
-      </Route>
-      <Route exact path="/board/:id">
-        <BoardPage />
-      </Route>
-      <Route exact path="/register">
-        <RegisterPage />
-      </Route>
-      
-      
-      
-      
-      
-      
-      
-        </Switch>
-
-
-
-
-
+        <Route exact path="/article/:id">
+          <ArticlePage />
+        </Route>
+        <Route exact path="/board/:id">
+          <BoardPage />
+        </Route>
+        <Route exact path="/register">
+          <RegisterPage />
+        </Route>
+      </Switch>
     </div>
-
-
-
-
-
-
   );
 }
